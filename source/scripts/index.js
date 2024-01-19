bc1be7d24ed4538abccc99e30df5ba9a2e4e7d80
Natalia-MA <Natalia-MA@users.noreply.github.com>
2024-01-19 07:14:03 +0400

    Corrections after video call

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,12 @@
 /* в этот файл добавляет скрипты*/
+const menuBtn = document.querySelctor(".burger");
+const menu = document.querySelctor(".main-header__navigation");
+
+if (document.querySelector(".no-js")) {
+  document.querySelector(".no-js").classList.remove("no-js");
+}
+
+menuBtn.addEventListener("click", (item) => {
+  item.preventDefault();
+  menu.classList.toggle("main-header__navigation--closed");
+});
