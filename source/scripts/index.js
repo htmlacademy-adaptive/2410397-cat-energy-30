98732167b0dea442f8e280b4e56f9c26e0a93c61
Natalia Melnik <melnik.tasha@yandex.ru>
2024-01-25 09:59:13 +0400

    Pre-call changes

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,39 +1,14 @@
 /* в этот файл добавляет скрипты*/
-const menu = document.querySelector('.main-header__navigation');
-const menuBtn = menu.querySelector('.burger');
+const menuBtn = document.querySelector('.burger');
+const menu = document.querySelector('.main-header__menu');
 
-if (document.querySelector('.main-header__menu--no-js')) {
-  document.querySelector('.main-header__menu--no-js').classList.remove('main-header__menu--no-js');
+if (document.querySelector('.no-js')) {
+  document.querySelector('.no-js').classList.remove('no-js');
 }
 
-menuBtn.addEventListener('click', (evt) => {
-  evt.preventDefault();
+menuBtn.addEventListener('click', (item) => {
+  item.preventDefault();
   menu.classList.toggle('is-open');
   menuBtn.classList.toggle('is-open');
 });
 
-// const menu = document.querySelector('.main-header__menu');
-// const menuBtn = menu.querySelector('.burger');
-// // const map = document.querySelector('.contacts__map');
-
-// menu.classList.remove('main-header__menu--no-js');
-// // map.classList.remove('contacts__map--no-js');
-
-// menuBtn.addEventListener('click', (evt) => {
-//   evt.preventDefault();
-//   menu.classList.toggle('is-open');
-//   menuBtn.classList.toggle('is-open');
-// });
-
-
-// const menuBtn = document.querySelector('.burger');
-// const menu = document.querySelector('.main-header__menu');
-
-// if (document.querySelector('.no-js')) {
-//   document.querySelector('.no-js').classList.remove('no-js');
-// }
-
-// menuBtn.addEventListener('click', (item) => {
-//   item.preventDefault();
-//   menu.classList.toggle('main-header__navigation--closed');
-// });
